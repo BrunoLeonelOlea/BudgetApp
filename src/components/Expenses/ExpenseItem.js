5ccdf78ac1f4f9f707e7b63620137e34a16a98ea
Bruno <bruno.olea@getronics.com>
2022-12-15 21:56:13 -0300

    ultimos cambios

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,14 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../layout/Card";
 
-const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-    const clickHandler = () => {
-        setTitle("Updated!");
-        console.log(title);
-    };
-
+const ExpenseItem = ({ title, date, amount }) => {
     return (
         <Card className="expense-item">
-            <ExpenseDate date={props.date} />
+            <ExpenseDate date={date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
+                <div className="expense-item__price">${amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Tittle</button>
         </Card>
     );
 };
